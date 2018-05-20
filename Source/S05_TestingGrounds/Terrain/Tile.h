0756368e40834d5a506db5e00ec26873e9a33b5b
NODZZ <tcorlett5@gmail.com>
2018-05-21 07:16:49 +0900

    TG80: Generating Random Numbers

---
+++ Source/S05_TestingGrounds/Terrain/Tile.h
@@ -0,0 +1,31 @@
+// Copyright Thomas Corlett 2018
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Tile.generated.h"
+
+UCLASS()
+class S05_TESTINGGROUNDS_API ATile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATile();
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	void PlaceActors();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
